1bacf87796d9b4eadda6cb064ca74ffac6214111
Armen G <gharibi.armen@yahoo.com>
2017-09-22 09:48:19 -0700

    Added notes and recipe entitys, setup the oneToOne relation b/w recipe and notes

--- src/main/java/com/agharibi/recipeapp/controllers/IndexController.java
+++ src/main/java/com/agharibi/recipeapp/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"/", "", "/index"})
     public String getIndexPage() {
+        System.err.println("re-run of spring dev tool");
         return "index";
     }
 }
---
+++ src/main/java/com/agharibi/recipeapp/domain/Notes.java
@@ -0,0 +1,44 @@
+package com.agharibi.recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    // String large object
+    // Hibernate JPA allow strings only up to 255 chars
+    // Lob annotation allows Hibernate to handle large string values > 255 chars
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
