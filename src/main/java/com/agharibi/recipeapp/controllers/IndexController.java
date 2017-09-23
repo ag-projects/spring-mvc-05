f6b42033b38c4e7cd0dbf07ba523bebc9da11591
Armen G <gharibi.armen@yahoo.com>
2017-09-22 20:18:20 -0700

    Added project lombok and utilized the lombok annotations in entities and spring beans.

--- src/main/java/com/agharibi/recipeapp/controllers/IndexController.java
+++ src/main/java/com/agharibi/recipeapp/controllers/IndexController.java
@@ -5,6 +5,7 @@ import com.agharibi.recipeapp.domain.UnitOfMeasure;
 import com.agharibi.recipeapp.repositories.CategoryRepository;
 import com.agharibi.recipeapp.repositories.UnitOfMeasureRepository;
 import com.agharibi.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -21,6 +23,7 @@ public class IndexController {
 
     @RequestMapping({"/", "", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Retrieving index page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
