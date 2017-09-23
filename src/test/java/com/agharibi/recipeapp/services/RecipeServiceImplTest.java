927d519a4bd915ddbd023e82c5fa8433205909e9
Armen G <gharibi.armen@yahoo.com>
2017-09-23 13:14:44 -0700

    Added a new show recipe detail page, unit tests for recipe service and contorller layers

--- src/test/java/com/agharibi/recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/agharibi/recipeapp/services/RecipeServiceImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -29,7 +30,22 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void getRecipes() throws Exception {
+    public void getRecipeByIdTest() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(12L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        Recipe returnedRecipe = recipeService.findById(12L);
+
+        assertNotNull("Null recipe returned", returnedRecipe);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipesTest() throws Exception {
         Recipe recipe = new Recipe();
         Set<Recipe> recipes = new HashSet<>();
         recipes.add(recipe);
