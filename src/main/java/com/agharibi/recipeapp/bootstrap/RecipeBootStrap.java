ae8ca40959070ed876a8631049e85007aaa44ad8
Armen G <gharibi.armen@yahoo.com>
2017-10-07 22:50:26 -0700

    Added MySQL properties for dev and prod profile

--- src/main/java/com/agharibi/recipeapp/bootstrap/RecipeBootStrap.java
+++ src/main/java/com/agharibi/recipeapp/bootstrap/RecipeBootStrap.java
@@ -5,6 +5,7 @@ import com.agharibi.recipeapp.repositories.CategoryRepository;
 import com.agharibi.recipeapp.repositories.RecipeRepository;
 import com.agharibi.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Profile("default")
 public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
